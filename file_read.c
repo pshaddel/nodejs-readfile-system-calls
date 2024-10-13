60624e88c89ff976e4cc2be129d34df6586f04b2
Poorshad Shaddel <p.shaddel@gmail.com>
2024-10-13 06:54:32 +0000

    better version of file_read use fopen - not use  fgetc

--- file_read.c
+++ file_read.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE 1024
+
 int main() {
     FILE *file;
-    char ch;
+    char buffer[BUFFER_SIZE];
+    size_t bytesRead;
 
     // Open the file in read mode
     file = fopen("hello.txt", "r");
@@ -14,9 +17,10 @@ int main() {
         return 1;
     }
 
-    // Read and log each character from the file until EOF
-    while ((ch = fgetc(file)) != EOF) {
-        putchar(ch);  // Output each character to the console
+    // Read and log chunks of data from the file until EOF
+    while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, file)) > 0)
+    {
+        fwrite(buffer, 1, bytesRead, stdout); // Output each chunk to the console
     }
 
     // Close the file
